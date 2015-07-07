e74d95332a57cdb5a4a9f947e170ae0b9f1cec49
John Doe <max.s32@i.ua>
2015-07-07 21:19:39 +0300

    Update and destroy task routes as standalone routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
  
   resources :projects do
-     resources :tasks
+     resources :tasks, except: [:update, :destroy]
   end  
+
+  resources :tasks, only: [:update, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
