a669386c903e7df6fefc2ade2350265f94534bbc
John Doe <max.s32@i.ua>
2015-07-08 22:46:28 +0300

    Add spring and rspec gems. Generate task's and project's test mockups.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,9 @@
 class Task < ActiveRecord::Base
     belongs_to :project
+
     validates :content, :priority, :deadline, :project_id, presence: true
-    validates :priority, uniqueness: { scope: :project_id }, numericality: { only_integer: true, greater_than: 0 }
+    validates :priority, :project_id, numericality: { only_integer: true, greater_than: 0 }
+    validates :priority, uniqueness: { scope: :project_id } 
 
     after_initialize :defaults
 
