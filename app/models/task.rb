b1fde0e93f49a31c9afba6b31da37b38c61ec6b5
John Doe <max.s32@i.ua>
2015-07-08 20:53:10 +0300

    Add some basic validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ActiveRecord::Base
     belongs_to :project
+    validates :content, :priority, :deadline, :project_id, presence: true
+    validates :project_id, uniqueness: { scope: :project_id }, numericality: { only_integer: true, greater_than: 0 }
+
+    after_initialize :defaults
+
+    def defaults
+        self.isdone ||= false
+    end
 end
